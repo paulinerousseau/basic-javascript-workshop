85deb483c83f51dfef24fefa0e2022afbaae3147
pauline <pauline_rousseau@hotmail.co.uk>
2016-07-05 18:47:15 +0000

    1 and 2

---
+++ answers.js
@@ -0,0 +1,32 @@
+
+
+//Write a function that takes a string 
+//and returns the first character of 
+//the string. Test your function on a 
+
+function firstLetter(string){
+    return string.substring(0,1);
+};
+
+firstLetter("cupboard");
+firstLetter(" ");
+
+
+//Write a function that takes a string 
+//and returns the last character of a string. 
+//Test your function on a few inputs, 
+//including the empty string.
+
+function lastLetter(string){
+    return string.substring(string.length-1);
+};
+                 
+lastLetter("cupboard");
+lastLetter(" ");
+    
+
+
+
+
+function 
+
